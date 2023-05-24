18060f1b7dec08fda411247c09eeee190cf5bb0c
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-05-24 14:38:40 -0700

    Initialize activityController

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+const activity_controller = require("../controllers/activityController")
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'OpenAirFit' });
-});
+router.get('/', activity_controller.index);
 
 router.get('/:city', function(req, res, next) {
   res.render('activities', {title: req.params.city});
