6aa07e64c8f59552bd166693dc4ad174916c839c
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-06-07 13:13:22 -0700

    Update routes for city controller

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,41 @@ var express = require('express');
 var router = express.Router();
 
 const index_controller = require("../controllers/indexController")
+const activity_controller = require("../controllers/activityController")
+const category_controller = require("../controllers/categoryController")
+const city_controller = require("../controllers/cityController")
+const organizer_controller = require("../controllers/organizerController")
+const participant_controller = require("../controllers/participantController")
 
 /* GET home page. */
 router.get('/', index_controller.index);
 
-// router.get('/:city', function(req, res, next) {
-//   res.render('activities', {title: req.params.city});
-// });
+// City routes
+// GET request for creating a City. NOTE This must come before routes that display City (uses id).
+router.get("/city/create", city_controller.city_create_get);
+
+// POST request for creating city.
+router.post("/city/create", city_controller.city_create_post);
+
+// GET request to delete city.
+router.get("/city/:id/delete", city_controller.city_delete_get);
+
+// POST request to delete city.
+router.post("/city/:id/delete", city_controller.city_delete_post);
+
+// GET request to update city.
+router.get("/city/:id/update", city_controller.city_update_get);
+
+// POST request to update city.
+router.post("/city/:id/update", city_controller.city_update_post);
+
+// GET request for one city.
+router.get("/city/:id", city_controller.city_detail);
+
+// GET request for list of all city items.
+router.get("/cities", city_controller.city_list);
+
+
 
 
 module.exports = router;
